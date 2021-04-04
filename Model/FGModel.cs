b14bf756dded19555c2bd0d403a41ed7446ee680
sarits991 <you@example.com>
2021-04-04 12:53:50 +0300

    temp

--- Model/FGModel.cs
+++ Model/FGModel.cs
@@ -5,7 +5,8 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
 
 namespace AnomalyDetection.Model
 {
@@ -15,7 +16,7 @@ namespace AnomalyDetection.Model
         private string csvPath;
         private string fgPath;
         public event PropertyChangedEventHandler PropertyChanged;
-       
+
         public string XmlPath
         {
             get { return xmlPath; }
@@ -59,13 +60,42 @@ namespace AnomalyDetection.Model
             }
             try
             {
-                IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
-                IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 5400);
+                // IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+                IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5400);
                 Socket socket = new Socket(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
                 socket.Connect(ipEndPoint);
+                //TcpClient client = new TcpClient();
+               // while (!client.Connected) // keep trynig to connect
+              //  {
+                 //   try { client.Connect(ipEndPoint); }
+                 //   catch (Exception e)
+                  //  {
+                  //  }
+              //  }
 
+
+
+
+
+               // BinaryWriter writer = new BinaryWriter(client.GetStream());
+                //   string[] lines = File.ReadAllLines(csvPath);
+                using (StreamReader sr = new StreamReader(csvPath))
+                {
+                    string currentLine;
+                    // currentLine will be null when the StreamReader reaches the end of file
+
+                    while ((currentLine = sr.ReadLine()) != null)
+                    {
+                        //currentLine += "\r\n";
+                       // writer.Write(Encoding.ASCII.GetBytes(currentLine));
+                        var byteArr = Encoding.ASCII.GetBytes(currentLine);
+                   
+                        socket.Send(byteArr);
+                        Thread.Sleep(100);
+                    }
+                }
             }
-            catch(Exception e)
+            catch (Exception e)
             {
 
             }
